118179e340d86e930a0d56d3f3690fd03bdb42d2
ArseniyKanev <arseniy.kan@gmail.com>
2024-05-26 18:37:17 +0300

    edit users

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers:  { registrations: "registrations", sessions: "sessions", passwords: "passwords"}
+  devise_for :users, skip: :registrations, controllers:  { sessions: "sessions", passwords: "passwords"}
 
   post "/upload_file" => "upload#upload_file", as: :upload_file
   post "/upload_image" => "upload#upload_image", as: :upload_image
-
   get "/download_file/:name" => "upload#access_file", as: :upload_access_file, name: /.*/
-
-
-  devise_scope :user do
-    get 'users/send_email', to: 'registrations#send_email'
-  end
   root 'tabs#show'
-
   get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale
   get '/admin', to: redirect('/admin/resources')
 
@@ -24,7 +17,7 @@ Rails.application.routes.draw do
   authenticate :user, :admin?.to_proc do
     namespace :admin do
       resources :tabs
-      resources :users, only: [:index, :show, :destroy]
+      resources :users, only: [:index, :show, :create, :new, :destroy, :edit, :update]
       resources :resources, only: :index
     end
   end
