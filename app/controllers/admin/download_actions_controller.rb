dfe8d9265d66d011c210b14c75e03ebf8d16bae8
ArseniyKanev <arseniy.kan@gmail.com>
2024-06-05 11:51:24 +0300

    add download action delete

--- app/controllers/admin/download_actions_controller.rb
+++ app/controllers/admin/download_actions_controller.rb
@@ -3,6 +3,11 @@ class Admin::DownloadActionsController < ApplicationController
   
     def index
       @download_actions = DownloadAction.order(created_at: :desc)
-    end  
+    end
+
+    def destroy
+      DownloadAction.find(params[:id]).destroy
+      redirect_to admin_download_actions_path
+    end
 end
   
