436229c112d998cf70d18468b76085fa77feaa0c
ArseniyKanev <arseniy.kan@gmail.com>
2025-02-09 00:06:36 +0300

    fix download actions

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,6 +1,8 @@
 class UploadController < ActionController::Base
   IMAGE_EXT = [".gif", ".jpeg", ".jpg", ".png", ".svg"]
 
+  before_action :set_current_directory, only: [:access_file]
+
   def upload_file
     if params[:file]
       FileUtils::mkdir_p(Rails.root.join("public/uploads/files"))
@@ -43,8 +45,19 @@ class UploadController < ActionController::Base
       ext = File.extname(params[:name])
       DownloadAction.new(filename: params[:name], user_id: current_user.id).save! unless (IMAGE_EXT.include?(ext) || ext == "")
       send_data File.read(Rails.root.join("public", "uploads", "files", params[:name])), disposition: "attachment"
+    elsif File.exist?(File.join(@current_directory, params[:name]))
+      ext = File.extname(params[:name])
+      DownloadAction.new(filename: params[:name], user_id: current_user.id).save! unless (IMAGE_EXT.include?(ext) || ext == "")
+      send_data File.read(File.join(@current_directory, params[:name])), disposition: "attachment"
     else
       render nothing: true
     end
+  end
+
+  private
+
+  def set_current_directory
+    base_directory = Rails.root.join('public', 'uploads', 'sharing')
+    @current_directory = params[:dir] ? File.join(base_directory, params[:dir]) : base_directory
   end
 end
